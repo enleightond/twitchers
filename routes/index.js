736358a2974180d2ceef915d5f930f48e3a6525d
enleightond <enleightond@yahoo.com>
2016-01-09 14:22:26 -0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Twitcher's Digest"  });
+  res.render('index', { title: "Twitcher's"  });
 });
 
 module.exports = router;
