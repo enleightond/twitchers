686edf32a352a67705ec903451f5728151248e8d
Jess Yang <jessyang650@users.noreply.github.com>
2016-01-13 16:00:38 -0800

    cleaned index.js, added migration schema, added pg to package.json

--- migrations/20160111112451_users.js
+++ migrations/20160111112451_users.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(table) {
+  	table.increments();
+  	table.integer('ID');
+  	table.string('NAME');
+  	table.string('EMAIL');
+  	table.string('PASSWORD');
+  	table.string('TWITCHID');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,10 @@ router.get('/dashboard', function(req, res, next) {
 router.get('/dashboard/search', function(req,res, next) {
 	var twrsQuery = req.query.twitcherSearch;
 
-	var twitchUrl = 'https://api.twitch.tv/kraken/search/games?q=' + twrsQuery + '&type=suggest'
-	var twitchUrl2 = 'https://api.twitch.tv/kraken/search/streams?q=' + twrsQuery + '&type=suggest'
-	var twitchUrl3 = 'https://api.twitch.tv/kraken/search/streams?q=' + twrsQuery + '&type=suggest'
-	var twitchUrl4 = 'https://api.twitch.tv/kraken/search/games?q=' + twrsQuery + '&type=suggest'
+	var twitchUrl = 'https://api.twitch.tv/kraken/search/games?q=' + twrsQuery + '&type=suggest';
+	var twitchUrl2 = 'https://api.twitch.tv/kraken/search/streams?q=' + twrsQuery + '&type=suggest';
+	var twitchUrl3 = 'https://api.twitch.tv/kraken/search/streams?q=' + twrsQuery + '&type=suggest';
+	var twitchUrl4 = 'https://api.twitch.tv/kraken/search/games?q=' + twrsQuery + '&type=suggest';
 	request.get(twitchUrl, function (error, response, body) {
 	  	var data = JSON.parse(body);
 
@@ -35,43 +35,10 @@ router.get('/dashboard/search', function(req,res, next) {
 
 
 					res.render('dashboard', {title: "Twitcher's Digest", games: data.games, streams: data.streamData.streams, streams: data.streamerData.streams, games: data.gameImg.games } );
-				})
-			})
-		})
-	})
-
-
-	// var twitchUrl = 'https://api.twitch.tv/kraken/search/games?q=' + twrsQuery + '&type=suggest'
-	// request.get(twitchUrl, function (error, response, body) {
-	//   	var data = JSON.parse(body);
-	//   	console.log(data)
-	// 	res.render('dashboard', {title: "Twitcher's Digest", streamers: data.streams.channel.display_name});
-	// })
-
+				});
+			});
+		});
+	});
 });
-
-<<<<<<< HEAD
-// $('.twitch-connect').click(function() {
-//   Twitch.login({
-//     scope: ['user_read', 'channel_read']
-//   });
-// })
-
-// Twitch.init({clientId: 'tm1iga98r4cytkq9wnaeup1j9m2iroe'}, function(error, status) {
-//   if (error) {
-//     // error encountered while loading
-//     console.log(error);
-//   }
-//   // the sdk is now loaded
-//   if (status.authenticated) {
-//     // user is currently logged in
-//   }
-=======
-
-// router.get('/dashboard/search/:searchString', function(req,res, next) {
-// 	var twrsSearch = req.params.searchString;
-// 	console.log("This is " + twrsSearch);
->>>>>>> 9dbb5b65b67bf1a9d023a5a1929da499bf41747c
-// });
 
 module.exports = router;
